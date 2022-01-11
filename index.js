c1bcd4fa84bb1673228832103defb202898105ef
Dalton <slattery.da@gmail.com>
2022-01-11 09:01:21 -0600

    commit tree c463c984e16039059a2bbc36e2b92cfbdb3d935e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    let distance = Math.abs(someValue - 42);
+    return distance;
+}
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    let n = Math.abs((destination - start) * 264);
+    return n;
+}
+function calculatesFarePrice(start, destination){
+    let x = distanceTravelledInFeet(start,destination);
+    let fare = 0;
+    if (x < 400){
+        return fare;
+    }
+    else if (x <= 2000){
+        fare = ((x - 400) * 0.02);
+        return fare;
+    }
+    else if (x <= 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
